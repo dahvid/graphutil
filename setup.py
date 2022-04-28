b44ef416434673b60be180a056a53e30973a436a
dahvid <david.minor1@ge.com>
2022-04-28 09:02:49 +0300

    for submission to pypi

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@
 from setuptools import setup
 
 setup(name='graphutil',
-      version='2.0',
+      version='2.1',
       description='Graph Processing Library',
       author='David Minor',
       author_email='dahvid.minor@gmail.com',
       url='https://github.com/dahvid/graphutil',
-      download_url="https://github.com/dahvid/graphutil/archive/refs/tags/v2.0.tar.gz",
+      download_url="https://github.com/dahvid/graphutil/archive/refs/tags/v2.1.tar.gz",
       keywords = ['graph'],
       classifiers= [],
       packages=['graphutil'],
